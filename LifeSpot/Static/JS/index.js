954c997ac0ca283ea104638fe01d808aeb50bb2c
Tiz <dannilo@bk.ru>
2025-03-17 09:47:57 +0400

    Добавьте файлы проекта.

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,75 @@
+﻿/*
+* Сессия теперь создается в общей области видимости.
+* Будет "захватываться" тремя функциями
+* 
+* */ 
+let session = new Map();
+
+const obj = {};
+
+/*
+* Сохранение данных сессии сразу при заходе пользователя на страницу
+*
+* */
+let session = {
+    'startDate': new Date().toLocaleString(),
+    'userAgent': window.navigator.userAgent,
+    'userAge': prompt("Пожалуйста, введите ваш возраст?")
+}
+
+/*
+* Проверка возраста пользователя
+*
+* */
+function checkAge() {
+    if (session.userAge >= 18) {
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+
+/*
+* Вывод данных сессии в консоль
+*
+* */
+let sessionLog = function () {
+    console.log('Начало сессии: ' + session.startDate)
+    console.log('Даныне клиента: ' + session.userAgent)
+    console.log('Возраст пользователя: : ' + session.userAge)
+}
+
+/*
+* Функция для фильтраци контента
+* Будет вызываться благодаря атрибуту oninput на index.html
+* 
+* */
+
+function filterContent(){
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++ ){
+        let videoText = elements[i].getElementsByTagName('h3')[0].innerText;
+
+        if(!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())){
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+
+/*
+* Всплывающее окно будет показано по таймауту
+* 
+* */
+// setTimeout(() =>
+//     alert("Нравится LifeSpot? " + '\n' +  "Подпишитесь на наш Instagram @lifespot999!" ),
+// 7000);
+
+
+
+
